8d5a933dc20a3a9edac6276e39cad54d7ba4f2b6
Ketyllen Oliveira <emyllykety@gmail.com>
2025-04-23 17:06:02 -0300

    chatbot implementation

--- infrastructure/src/main/java/com/terracota/infrastructure/api/controller/ChatbotController.java
+++ infrastructure/src/main/java/com/terracota/infrastructure/api/controller/ChatbotController.java
@@ -1,4 +1,24 @@
 package com.terracota.infrastructure.api.controller;
 
+import com.terracota.infrastructure.chatbot.ChatbotService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
+
+@RestController
+@RequestMapping("chatbot")
 public class ChatbotController {
+    private final ChatbotService chatbotService;
+
+    public ChatbotController(final ChatbotService chatbotService) {
+        this.chatbotService = Objects.requireNonNull(chatbotService);
+    }
+
+    @PostMapping
+    public String send(@RequestBody String prompt) throws Exception {
+        return chatbotService.processMessage(prompt);
+    }
 }
